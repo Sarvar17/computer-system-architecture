04d70b92496f7e1eb2a590889dda15f288823a5e
Sarvar17 <70842478+Sarvar17@users.noreply.github.com>
2021-10-24 15:00:03 +0300

    Update run_tests.sh

--- homework_2/run_tests.sh
+++ homework_2/run_tests.sh
@@ -1,20 +1,20 @@
 echo "\nRun test #0"
-time task-1/code/out/homework_2 -f task-1/tests/test00.txt task-1/tests/out00_1.txt task-1/tests/out00_2.txt
+time task-2/code/out/homework_2 -f task-2/tests/test00.txt task-2/tests/out00_1.txt task-2/tests/out00_2.txt
 
 echo "\nRun test #1"
-time task-1/code/out/homework_2 -f task-1/tests/test01.txt task-1/tests/out01_1.txt task-1/tests/out01_2.txt
+time task-2/code/out/homework_2 -f task-2/tests/test01.txt task-2/tests/out01_1.txt task-2/tests/out01_2.txt
 
 echo "\nRun test #2"
-time task-1/code/out/homework_2 -f task-1/tests/test02.txt task-1/tests/out02_1.txt task-1/tests/out02_2.txt
+time task-2/code/out/homework_2 -f task-2/tests/test02.txt task-2/tests/out02_1.txt task-2/tests/out02_2.txt
 
 echo "\nRun test #3"
-time task-1/code/out/homework_2 -f task-1/tests/test03.txt task-1/tests/out03_1.txt task-1/tests/out03_2.txt
+time task-2/code/out/homework_2 -f task-2/tests/test03.txt task-2/tests/out03_1.txt task-2/tests/out03_2.txt
 
 echo "\nRun test #4"
-time task-1/code/out/homework_2 -f task-1/tests/test04.txt task-1/tests/out04_1.txt task-1/tests/out04_2.txt
+time task-2/code/out/homework_2 -f task-2/tests/test04.txt task-2/tests/out04_1.txt task-2/tests/out04_2.txt
 
 echo "\nRun test #5"
-time task-1/code/out/homework_2 -f task-1/tests/test05.txt task-1/tests/out05_1.txt task-1/tests/out05_2.txt
+time task-2/code/out/homework_2 -f task-2/tests/test05.txt task-2/tests/out05_1.txt task-2/tests/out05_2.txt
 
 echo "\nRun test #6 (random)"
-time task-1/code/out/homework_2 -n 10000 task-1/tests/outrnd_1.txt task-1/tests/outrnd_2.txt
+time task-2/code/out/homework_2 -n 10000 task-2/tests/outrnd_1.txt task-2/tests/outrnd_2.txt
